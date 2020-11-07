99228c9217622e9431b03eb321d32bfac47696e8
burshtynv <vanyaburshtyn@gmail.com>
2020-11-07 14:50:17 +0300

    modify project

--- app.js
+++ app.js
@@ -6,20 +6,47 @@ const empty = {
     top: 0
 };
 
-for (let i = 1; i <= 15; i++){
-    const cell =  document.createElement('div');
+const cells = [];
+cells.push(empty);
+
+function move(index) {
+    const cell = cells[index];
+
+    cell.element.style.left = `${empty.left * cellSize}px`;
+    cell.element.style.top = `${empty.top * cellSize}px`;
+
+    const emptyLeft = empty.left;
+    const emptyTop = empty.top;
+    empty.left = cell.left;
+    empty.top = cell.top;
+    cell.left = emptyLeft;
+    cell.top = emptyTop;
+
+
+}
+
+for (let i = 1; i <= 15; i++) {
+    const cell = document.createElement('div');
     cell.className = 'cell';
     cell.innerHTML = i;
 
-
-
-
     const left = i % 4;
-    const top = (i - left) / 4;  
+    const top = (i - left) / 4;
 
-    cell.style.left = `${left * cellSize}px`
-    cell.style.top = `${top * cellSize}px`
+    cells.push({
+        left: left,
+        top: top,
+        element: cell
+    });
 
+
+
+    cell.style.left = `${left * cellSize}px`;
+    cell.style.top = `${top * cellSize}px`;
 
     field.append(cell);
+
+    cell.addEventListener('click', () => {
+        move(i);
+    })
 }
